5547880bf8d6daf424251feca8f4cc7dff4e7ff9
Unknown <sam.copp8@gmail.com>
2018-06-05 16:12:03 +0200

    ChatBot working

--- src/App.js
+++ src/App.js
@@ -1,31 +1,84 @@
 import React, { Component } from 'react';
-import { Widget, addResponseMessage  } from 'react-chat-widget';
-import 'react-chat-widget/lib/styles.css';
-class App extends Component {
+import ChatBot, { Loading } from 'react-simple-chatbot';
 
-  componentDidMount() {
-    addResponseMessage("Welcome to this awesome chat!");
-}
+class DBPedia extends Component {
+  constructor(props) {
+    super(props);
 
-  handleNewUserMessage = (newMessage) => {
-  console.log(`New message incomig! ${newMessage}`);
-  // Now send the message throught the backend API
-}
+    this.state = {
+      loading: true,
+      result: '',
+      trigger: false,
+    };
+  }
+
+  componentWillMount() {
+    const self = this;
+    const { steps, previousStep } = this.props;
+    const search = steps.search.value;
+    const endpoint = encodeURI('http://localhost:6060/api/message');
+    var headers = new Headers();
+    var message = {
+        "message": search
+    };
+    // Tell the server we want JSON back
+    headers.set('Content-Type', 'application/json');
+    var fetchOptions = {
+       method: 'POST',
+       headers,
+       body: JSON.stringify(message)
+     };
+     var responsePromise = fetch(endpoint, fetchOptions);
+     // 3. Use the response
+      // ================================
+      responsePromise
+          // 3.1 Convert the response into JSON-JS object.
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(jsonData) {
+          self.setState({
+            result: jsonData,
+          })
+            self.props.triggerNextStep(4);
+        });;
+  }
+
+
 
   render() {
+    const { trigger, loading, result } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="App">
-          <Widget
-          handleNewUserMessage={this.handleNewUserMessage}
-         />
-        </div>
+      <div>
+         {this.state.result}
       </div>
     );
   }
 }
+
+const App = () => (
+  <ChatBot
+    steps={[
+      {
+        id: '1',
+        message: 'Type something to search on Wikipédia. (Ex.: Brazil)',
+        trigger: 'search',
+      },
+      {
+        id: 'search',
+        user: true,
+        trigger: '3',
+      },
+      {
+        id: '3',
+        component: <DBPedia />,
+        asMessage: true,
+        waitAction: true,
+        trigger: 'search',
+      },
+    ]}
+  />
+);
 
 export default App;
