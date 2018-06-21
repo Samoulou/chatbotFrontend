522cd345a41a78b21689dd218043a088ec0c99bf
Unknown <sam.copp8@gmail.com>
2018-06-21 09:01:56 +0200

    refactoring + type

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ThemeProvider } from 'styled-components';
 import ChatBot, { Loading } from 'react-simple-chatbot';
+import CustomAnswer from './CustomAnswer';
 const theme = {
    backgroundImage: 'url("https://www.bookmee.ch/45cf21b9ee27587b7ea6a87fc033f899.svg")',
    fontFamily: '“Helvetica Neue”,Helvetica,Arial,sans-serif!important',
@@ -13,81 +14,6 @@ const theme = {
    userFontColor: 'rgb(110, 122, 137)',
 
 };
-
-class CustomAnswer extends Component {
-
-  // TODO: METTRE COMMENTAIRES
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      result: '',
-      trigger: '',
-    };
-  }
-
-  // TODO: METTRE COMMENTAIRES
-  componentWillMount() {
-    const self = this;
-    const answerText = 1;
-    const nextStep = {value:null, trigger:'options'}
-    const { steps, previousStep, step } = this.props;
-    const search = steps.search.value;
-    const endpoint = encodeURI('http://localhost:6060/api/message');
-    var headers = new Headers();
-    // var options = steps.options;
-    // console.log(options);
-    // if(options != null){
-    //   if(steps.options.value === 'celibataire' || steps.options.value === 'marie'){
-    //     var message = {
-    //       "message" : steps.options.message,
-    //     }
-    //     options = null;
-    //     console.log(options);
-    //   }
-    // }
-    // else{
-      var message = {
-          "message": search
-      };
-    //}
-    // Tell the server we want JSON back
-    headers.set('Content-Type', 'application/json');
-    console.log(message);
-    var fetchOptions = {
-       method: 'POST',
-       headers,
-       body: JSON.stringify(message)
-     };
-
-     // TODO: METTRE COMMENTAIRES
-     var responsePromise = fetch(endpoint, fetchOptions);
-     // 3. Use the response
-     responsePromise
-    // 3.1 Convert the response into JSON-JS object.
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(jsonData) {
-      // if(jsonData[0][0].value === 'price_information'){
-      //   self.props.triggerNextStep(nextStep)
-      // }
-      self.setState({
-        result: jsonData[answerText],
-      })
-      self.props.triggerNextStep();
-    });
-  }
-
-  render() {
-    const { trigger, loading, result } = this.state;
-    return (
-      <div>
-         {this.state.result}
-      </div>
-    );
-  }
-}
 
 const App = () => (
   <ThemeProvider theme={theme}>
---
+++ src/CustomAnswer.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import ChatBot, { Loading } from 'react-simple-chatbot';
+
+class CustomAnswer extends Component {
+
+  // TODO: METTRE COMMENTAIRES
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      result: '',
+      trigger: '',
+      type:'',
+    };
+  }
+
+  // TODO: METTRE COMMENTAIRES
+  componentWillMount() {
+    const self = this;
+    const answerText = 1;
+    const type = 2;
+    const nextStep = {value:null, trigger:'options'}
+    const { steps, previousStep, step } = this.props;
+    const search = steps.search.value;
+    const endpoint = encodeURI('http://localhost:6060/api/message');
+    var headers = new Headers();
+    // var options = steps.options;
+    // console.log(options);
+    // if(options != null){
+    //   if(steps.options.value === 'celibataire' || steps.options.value === 'marie'){
+    //     var message = {
+    //       "message" : steps.options.message,
+    //     }
+    //     options = null;
+    //     console.log(options);
+    //   }
+    // }
+    // else{
+      var message = {
+          "message": search
+      };
+    //}
+    // Tell the server we want JSON back
+    headers.set('Content-Type', 'application/json');
+    console.log(message);
+    var fetchOptions = {
+       method: 'POST',
+       headers,
+       body: JSON.stringify(message)
+     };
+
+     // TODO: METTRE COMMENTAIRES
+     var responsePromise = fetch(endpoint, fetchOptions);
+     // 3. Use the response
+     responsePromise
+    // 3.1 Convert the response into JSON-JS object.
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(jsonData) {
+      // if(jsonData[0][0].value === 'price_information'){
+      //   self.props.triggerNextStep(nextStep)
+      // }
+      console.log(jsonData);
+      self.setState({
+        result: jsonData[answerText],
+        type: jsonData[type],
+      })
+      self.props.triggerNextStep();
+    });
+  }
+
+  renderAnswer(){
+    const { trigger, loading, result } = this.state;
+    let answer;
+    if (this.state.type === 'text') {
+      answer = <div>{this.state.result}</div>;
+    } else if(this.state.type === 'url') {
+      answer = <a href={this.state.result}>{this.state.result}</a>
+      }
+      return answer;
+    }
+
+  render() {
+    return (
+        <div>
+           {this.renderAnswer()}
+        </div>
+    );
+  }
+}
+
+export default CustomAnswer;
